2341e3a1fb6a18253b068aa647c28fb7ec725307
fardin-faisal <fardin00faisal05@gmail.com>
2023-06-15 21:05:00 +0600

    backend

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,19 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.homePage),
+    path('login/', views.loginPage),
+    path('signup/', views.signupPage),
+    path('blog/', views.blogPage),
+    path('blog/add/', views.blogAddPage),
+    path('blog/<str:pk>/', views.blogDetails),
+    path('blog/edit/<str:pk>/', views.blogEditPage),
+    path('blog/delete/<str:pk>/', views.blogDelete),
+    path('comment/delete/<str:pk>/', views.commentDelete),
+    path('create-user/', views.createUser),
+    path('login-user/', views.loginUser),
+    path('profile/edit/', views.profileEditPage),
+    path('profile/<str:pk>/', views.profilePage),
+    path('logout/', views.logoutUser),
 ]
